fb0a2127384bb22f46d5b0ebe6e5805f698b17fd
boualo <meryembouali262@gmail.com>
2024-12-26 23:48:37 +0100

    update

--- web of science/Untitled-1.sh
+++
@@ -1,6 +0,0 @@
- val rowsRdd  : RDD[Row] = sc.parallelize(…) 
-      val schema = StructType( 
-StructField("field_1", Type_Field_1, nullable = [True or False] ), 
-StructField("field_2", Type_Field_2, nullable = [True or False] ), 
-      )      
-      sc.createDataFrame(rowsRdd, schema)
